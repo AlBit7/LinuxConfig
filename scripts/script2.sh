421b2c962d67dbe7e5f38ee8a4877eaa1ef0b11b
AlBit7 <72737404+AlBit7@users.noreply.github.com>
2022-03-01 21:37:19 +0100

    Update script2.sh

--- scripts/script2.sh
+++ scripts/script2.sh
@@ -1,6 +1,7 @@
+gnome-extensions enable user-theme@gnome-shell-extensions.gcampax.github.com
+gnome-extensions disable ubuntu-dock@ubuntu.com 
+
 cd
 mkdir .themes
-gnome-extensions enable user-theme@gnome-shell-extensions.gcampax.github.com
-
 echo "scaricare questo pacchetto, estrarlo ed inserire i file nella directoory $(pwd)/.themes"
 firefox https://www.pling.com/p/1357889
