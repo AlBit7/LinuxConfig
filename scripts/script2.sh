c389a295813162db035159c062395d53fb1af414
AlBit7 <72737404+AlBit7@users.noreply.github.com>
2022-03-01 21:52:35 +0100

    Update script2.sh

--- scripts/script2.sh
+++ scripts/script2.sh
@@ -1,7 +1,10 @@
+git clone https://github.com/vinceliuice/Orchis-theme.git
+cd Orchis-theme
+chmod +x install.sh
+./install.sh
+cd ..
+rm -r Orchis-theme
+
 gnome-extensions enable user-theme@gnome-shell-extensions.gcampax.github.com
 gnome-extensions disable ubuntu-dock@ubuntu.com 
-
-cd
-mkdir .themes
-echo "scaricare questo pacchetto, estrarlo ed inserire i file nella directoory $(pwd)/.themes"
-firefox https://www.pling.com/p/1357889
+gnome-tweaks
