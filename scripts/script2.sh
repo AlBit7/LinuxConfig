83bd43bc929d6b59432fb1d1bb1d05899b421193
AlBit7 <72737404+AlBit7@users.noreply.github.com>
2022-03-01 21:34:46 +0100

    Create script2.sh

---
+++ scripts/script2.sh
@@ -0,0 +1,6 @@
+cd
+mkdir .themes
+gnome-extensions enable user-theme@gnome-shell-extensions.gcampax.github.com
+
+echo "scaricare questo pacchetto, estrarlo ed inserire i file nella directoory $(pwd)/.themes"
+firefox https://www.pling.com/p/1357889
