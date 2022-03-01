059239f888dd7a45cc996b81378bf90be3fe406d
AlBit7 <72737404+AlBit7@users.noreply.github.com>
2022-03-01 21:18:53 +0100

    Update installa.sh

--- scripts/installa.sh
+++ scripts/installa.sh
@@ -1,1 +1,16 @@
+cd
+echo "nella directory: $(pwd)"
+if [[ ! $(sudo echo 0) ]]; then exit; fi
 
+echo "installando gnome-tweak-tool..."
+sudo apt install gnome-tweak-tool 
+echo "OK!"
+echo "installando gnome-shell-exstentions..."
+sudo apt install gnome-shell-exstentions 
+echo "OK!"
+echo "installando chrome-gnome-shell..."
+sudo apt install chrome-gnome-shell
+echo "OK!"
+
+echo "apro browser: installa l'estensione"
+firefox https://extensions.gnome.org/
